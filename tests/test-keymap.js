2f8901f11446a29c0ced94a9d1d9cda794f29d9e
Mat Taylor <mat@MacBook-Pro.local>
2022-01-13 19:18:02 -0800

    more realistic perfTests for keyMap comparisons

--- tests/test-keymap.js
+++ tests/test-keymap.js
@@ -1,6 +1,7 @@
 import { Encoder } from '../index.js'
 import assert from 'assert'
 import { Console } from 'console'
+import cborExtract from 'cbor-extract'
 
 const small = [ 
 	{ bn: '/3303/0/5700', bt: 1278887, v: 35.5 },{ t: 10, v: 34 },{ t: 20, v: 33 },{ t: 30, v: 32 },{ t: 40, v: 31 },{ t: 50, v: 30 } 
@@ -13,29 +14,45 @@ for (let i = 0; i < 1000; i++) large.push({ t: 100+i, n: '1', vs: 'value-'+i } )
 
 let senmlKeys = { bs: -6, bv: -5, bu: -4, bt: -3, bn: -2, n: 0, u: 1, v: 2, vs: 3, t: 6, ut: 7, vd: 8 }
 
-function preMap(data) { 
+function reverse(obj) {
+  let rev = new Map()
+	for (let [k,v] of Object.entries(obj)) rev.set(v,k)
+  return rev
+}
+
+let senmlRevs = reverse(senmlKeys)
+
+function preMapDecode(maps) { 
+  let data = []
+  for (let map of maps) {
+    let item = {}
+    map.forEach((v,k) => item[senmlRevs.has(k) ? senmlRevs.get(k) : k] =  v)
+		data.push(item)
+  }
+  return data
+}
+
+function preMapEncode(data) { 
   let maps = []
   for (let r of data) { 
     let map = new Map()
     Object.entries(r).map(([k,v]) => map.set(senmlKeys[k], v))
     maps.push(map)
   }
-  console.log(maps[0])
   return maps
 }
 
 function perfTest(data, label) {
   let basic = test(data, {useRecords: false})
-  let maps = preMap(data)
+  //let maps = preMap(data)
   compare(`Basic  No Recs: ${label}`, basic, basic)
-  compare(`PreMap No Recs: ${label}`, test(maps, {useRecords: false}), basic)
+  compare(`PreMap No Recs: ${label}`, test(data, {useRecords: false}, true), basic)
   compare(`KeyMap No Recs: ${label}`, test(data, {useRecords: false, keyMap: senmlKeys}), basic)
   compare(`Optima No Recs: ${label}`, test(data, {useRecords: false, _keyMap: senmlKeys}), basic)
   compare(`Basic  Wi Recs: ${label}`, test(data, {useRecords: true}), basic)
-  compare(`PreMap Wi Recs: ${label}`, test(maps, {useRecords: true}), basic)
+  compare(`PreMap Wi Recs: ${label}`, test(data, {useRecords: true}, true), basic)
   compare(`KeyMap Wi Recs: ${label}`, test(data, {useRecords: true,  keyMap: senmlKeys}), basic) 
   compare(`Optima Wi Recs: ${label}`, test(data, {useRecords: true,  _keyMap: senmlKeys}), basic) 
-   
  }
 
 function compare(label, r1, r2) {
@@ -47,15 +64,15 @@ function compare(label, r1, r2) {
   console.log(`  Decode: ${pct(r2.decAvg, r1.decAvg)}% \t(${r1.decAvg})`)
 }
 
-function test(data, opts, its=1000) { 
+function test(data, opts, preMap, its=1000) { 
   let cbor = new Encoder(opts)
-  let buff = cbor.mapEncode(data)
+  let buff = preMap ? cbor.encode(preMapEncode(data)) : cbor.mapEncode(data)
   let t1 = Date.now()
-  for (let i = 0; i < its; i++) cbor.mapEncode(data)
+  for (let i = 0; i < its; i++) preMap ? cbor.encode(preMapEncode(data)) : cbor.mapEncode(data)
   let t2 = Date.now()
-  for (let i = 0; i < its; i++) cbor.mapDecode(buff)
+  for (let i = 0; i < its; i++) preMap ? preMapDecode(cbor.decode(buff)) : cbor.mapDecode(buff)
   let t3 = Date.now()
-  assert.deepEqual(cbor.mapDecode(buff), data)
+  assert.deepEqual(preMap ? preMapDecode(cbor.decode(buff)) : cbor.mapDecode(buff), data)
   return {bufLen: buff.length, encAvg: (t2-t1)/its, decAvg: (t3-t2)/its }
 }
 
