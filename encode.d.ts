b700b753be4b93911876752114bc7a0986913a90
Kris Zyp <kriszyp@gmail.com>
2022-08-22 07:08:49 -0600

    Add support for node16 module resolution in TS

--- encode.d.ts
+++ encode.d.ts
@@ -1,6 +1,1 @@
-import { Decoder } from './decode'
-export { addExtension, FLOAT32_OPTIONS } from './decode'
-export class Encoder extends Decoder {
-	encode(value: any): Buffer
-}
-export function encode(value: any): Buffer
+export { encode, Encoder, addExtension, FLOAT32_OPTIONS } from '.'
--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,54 @@
-import { Options } from './decode'
-export { Decoder, decode, addExtension, FLOAT32_OPTIONS, clearSource, roundFloat32, isNativeAccelerationEnabled } from './decode'
-export { Encoder, encode } from './encode'
+export enum FLOAT32_OPTIONS {
+	NEVER = 0,
+	ALWAYS = 1,
+	DECIMAL_ROUND = 3,
+	DECIMAL_FIT = 4
+}
+
+export interface Options {
+	useFloat32?: FLOAT32_OPTIONS
+	useRecords?: boolean
+	structures?: {}[]
+	structuredClone?: boolean
+	mapsAsObjects?: boolean
+	variableMapSize?: boolean
+	copyBuffers?: boolean
+	bundleStrings?: boolean
+	useTimestamp32?: boolean
+	largeBigIntToFloat?: boolean
+	encodeUndefinedAsNil?: boolean
+	maxSharedStructures?: number
+	maxOwnStructures?: number
+	useSelfDescribedHeader?: boolean
+	keyMap?: {}
+	shouldShareStructure?: (keys: string[]) => boolean
+	getStructures?(): {}[]
+	saveStructures?(structures: {}[]): boolean | void
+	onInvalidDate?: () => any
+}
+interface Extension {
+	Class: Function
+	tag: number
+	encode(value: any): Buffer | Uint8Array
+	decode(messagePack: Buffer | Uint8Array): any
+}
+export class Decoder {
+	constructor(options?: Options)
+	decode(messagePack: Buffer | Uint8Array): any
+	decodeMultiple(messagePack: Buffer | Uint8Array, forEach?: (value: any) => any): [] | void
+}
+export function decode(messagePack: Buffer | Uint8Array): any
+export function decodeMultiple(messagePack: Buffer | Uint8Array, forEach?: (value: any) => any): [] | void
+export function addExtension(extension: Extension): void
+export function clearSource(): void
+export function roundFloat32(float32Number: number): number
+export let isNativeAccelerationEnabled: boolean
+
+export class Encoder extends Decoder {
+	encode(value: any): Buffer
+}
+export function encode(value: any): Buffer
+
 import { Transform, Readable } from 'stream'
 
 export as namespace CBOR;
